fe00dacd7193e4e8d53ef209e2f52cbeb4cca372
Carlitos <carlitosdutan@pursuit.org>
2023-10-16 10:31:46 -0400

    updateSalePrice and deletePhone functions completed

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const { readJSONFile, writeJSONFile } = require("./src/helpers");
-const { create, checkInStock } = require("./src/phoneController");
+const { createPhone, checkInStock, updateSalePrice, deletePhone } = require("./src/phoneController");
 
 function run () {
     const action = process.argv[2];
-    // const phoneId = process.argv[3];
+    const phoneId = process.argv[3];
 
     let writeToFile = false;
     let updatedPhones = [];
 
     switch(action){
         case 'create':
-            updatedPhones = create();
+            updatedPhones = createPhone();
             writeToFile = true;
             break;
         case 'show':
             updatedPhones = checkInStock();
+            writeToFile = true;
+            break;
+        case 'update':
+            updatedPhones = updateSalePrice(phoneId,process.argv[4]);
+            writeToFile = true;
+            break;
+        case 'destroy':
+            updatedPhones = deletePhone(phoneId);
             writeToFile = true;
             break;
     }
--- src/phoneController.js
+++ src/phoneController.js
@@ -3,8 +3,9 @@ const { readJSONFile, writeJSONFile } = require("./helpers");
 
 const phones = readJSONFile("./data", "phones.json");
 
-function create(){
-    const [,,,name,priceInCents,color,storage,inStock] = process.argv;
+const [,,,name,priceInCents,color,storage,inStock] = process.argv;
+
+function createPhone(){
     const phone = {
         id: nanoid(4),
         name: name,
@@ -12,6 +13,7 @@ function create(){
         color: color,
         storage: storage,
         inStock: inStock,
+        onSale: false,
     };
     phones.push(phone);
     return phones;
@@ -25,26 +27,48 @@ function checkInStock () {
     return phones.filter((phone) => phone.inStock === true);
 }
 
-
-
 // UPDATE
 // Function user can update the price on any cellphone. By inputting a price "npm run update 999." Will check if onSale is true
 // USER INPUT 999
 
+// Function iterate through phones and check if user input key for onSale is true. If it is allow them to update the price
 
+function updateSalePrice (phoneId, updatedPrice) {
+    // Check if inputted id matches any phone's id by using the .find() method
+    const targetPhone = phones.find((phone) => {
+        if(phoneId === phone.id){
+            return phone;
+        }
+    });
+    // If it doesnt match return error message
+    if(!targetPhone){
+        console.log("No phone was found");
+    }
+    // If it matches allow them to update the price
+    else if(targetPhone){
+        targetPhone.priceInCents = updatedPrice;
+        targetPhone.onSale = true;
+        console.log(`${targetPhone.name} ${targetPhone.storage}GB ${targetPhone.color} price was successfully updated to $${updatedPrice}!`);
+    }
+    return phones;
+}
 
+function deletePhone(phoneId) {
+    const index = phones.findIndex(phone => phone.id === phoneId);
 
-
-
-
-
-
-
-
-
-
+    if(index > -1){
+        phones.splice(index,1);
+        return phones;
+    }
+    else{
+        console.log("Phone was not found");
+        return phones;
+    }
+}
 
 module.exports = {
-    create,
+    createPhone,
     checkInStock,
+    updateSalePrice,
+    deletePhone,
 };
